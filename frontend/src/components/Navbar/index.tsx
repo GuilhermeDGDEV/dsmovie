66215ad21cd6ae3277cf8ff4120d17d52e7eb687
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2022-01-10 20:29:41 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+  return (
+    <header className="d-flex align-items-center">
+      <nav className="container">
+        <div className="d-flex justify-content-between text-light dsmovie-nav-content">
+          <h1 className="m-0">DSMovie</h1>
+          <a href="https://github.com/GuilhermeDGDEV" className="d-flex align-items-center">
+            <div className="d-flex align-items-center">
+              <GithubIcon />
+              <p className="dsmovie-contanct-link">/GuilhermeDGDEV</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
